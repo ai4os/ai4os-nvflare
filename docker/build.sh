4631fa78281ae8e1b7d882ec8fa8253dde2975f0
stevo <stefan.dlugolinsky@savba.sk>
2025-03-01 01:43:21 +0100

    added NVF_REPO as build arg

--- docker/build.sh
+++ docker/build.sh
@@ -2,19 +2,22 @@
 
 SOURCE=${1:-"default.env"}
 
+set -a
 source ${SOURCE}
+set +a
 
 #
 # server image
 #
 
 docker build \
-	--build-arg NVF_BASE_IMAGE_SERVER=${NVF_BASE_IMAGE_SERVER} \
-	--build-arg NVF_REPO=${NVF_REPO} \
-	--build-arg NVF_VERSION=${NVF_VERSION} \
-	-t sht3v0/ai4-nvflare-server:$NVF_VERSION \
-	-f Dockerfile-server \
-	.
+  --build-arg NVF_REPO=${NVF_REPO} \
+  --build-arg NVF_BASE_IMAGE_SERVER=${NVF_BASE_IMAGE_SERVER} \
+  --build-arg NVF_REPO=${NVF_REPO} \
+  --build-arg NVF_VERSION=${NVF_VERSION} \
+  -t sht3v0/ai4-nvflare-server:$NVF_VERSION \
+  -f Dockerfile-server \
+  .
 
 docker push sht3v0/ai4-nvflare-server:$NVF_VERSION
 
@@ -23,12 +26,13 @@ docker push sht3v0/ai4-nvflare-server:$NVF_VERSION
 #
 
 docker build \
-	--build-arg NVF_BASE_IMAGE_CLIENT=${NVF_BASE_IMAGE_CLIENT} \
-	--build-arg NVF_REPO=${NVF_REPO} \
-	--build-arg NVF_VERSION=${NVF_VERSION} \
-	-t sht3v0/ai4-nvflare-client:$NVF_VERSION \
-	-f Dockerfile-client \
-	.
+  --build-arg NVF_REPO=${NVF_REPO} \
+  --build-arg NVF_BASE_IMAGE_CLIENT=${NVF_BASE_IMAGE_CLIENT} \
+  --build-arg NVF_REPO=${NVF_REPO} \
+  --build-arg NVF_VERSION=${NVF_VERSION} \
+  -t sht3v0/ai4-nvflare-client:$NVF_VERSION \
+  -f Dockerfile-client \
+  .
 
 docker push sht3v0/ai4-nvflare-client:$NVF_VERSION
 
@@ -37,10 +41,11 @@ docker push sht3v0/ai4-nvflare-client:$NVF_VERSION
 #
 
 docker build \
-	--build-arg NVF_BASE_IMAGE_DASHBOARD=${NVF_BASE_IMAGE_DASHBOARD} \
-	--build-arg NVF_VERSION=${NVF_VERSION} \
-	-t sht3v0/ai4-nvflare-dashboard:$NVF_VERSION \
-	-f Dockerfile-dashboard \
-	.
+  --build-arg NVF_REPO=${NVF_REPO} \
+  --build-arg NVF_BASE_IMAGE_DASHBOARD=${NVF_BASE_IMAGE_DASHBOARD} \
+  --build-arg NVF_VERSION=${NVF_VERSION} \
+  -t sht3v0/ai4-nvflare-dashboard:$NVF_VERSION \
+  -f Dockerfile-dashboard \
+  .
 
 docker push sht3v0/ai4-nvflare-dashboard:$NVF_VERSION
